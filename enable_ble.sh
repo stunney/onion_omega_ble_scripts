2ea0629dd118704d9fee47c3772c6152124cd933
S. Tunney <stephen.tunney@gmail.com>
2020-05-29 16:22:10 -0400

    Update enable_ble.sh
    Do it all in one line!

--- enable_ble.sh
+++ enable_ble.sh
@@ -1,6 +1,4 @@
 #Your device ID might be different, update "hci0" to the device ID given on your system if you need to.
 
-hciconfig -a
-hciconfig hci0 up
-hciconfig hci0 sspmode enable
-hciconfig hci0 piscan
+
+hciconfig hci0 up && hciconfig hci0 sspmode enable && hciconfig hci0 piscan && hciconfig -a
